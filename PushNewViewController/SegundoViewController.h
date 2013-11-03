875a60002b56fdb97cde4b819d5e1afd84c5824f
Maikol Araya Esquivel <maraya@lacreativeria.com>
2013-11-02 19:05:50 -0600

    first commit

---
+++ PushNewViewController/SegundoViewController.h
@@ -0,0 +1,13 @@
+//
+//  SegundoViewController.h
+//  PushNewViewController
+//
+//  Created by Maikol Araya on 11/2/13.
+//  Copyright (c) 2013 La Creativeria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SegundoViewController : UIViewController
+
+@end
---
+++ PushNewViewController/SegundoViewController.m
@@ -0,0 +1,38 @@
+//
+//  SegundoViewController.m
+//  PushNewViewController
+//
+//  Created by Maikol Araya on 11/2/13.
+//  Copyright (c) 2013 La Creativeria. All rights reserved.
+//
+
+#import "SegundoViewController.h"
+
+@interface SegundoViewController ()
+
+@end
+
+@implementation SegundoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
